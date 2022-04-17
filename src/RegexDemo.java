f43edf59952b29db56e809296ab18928b4458c48
classroom <himanshukanoje23@gmail.com>
2022-04-17 17:35:14 +0530

    my commit

--- src/RegexDemo.java
+++ src/RegexDemo.java
@@ -39,7 +39,7 @@ public static void validlast(String str1)
 }
 public static void validemail(String str1)
 {
-	String regex="[a-zA-Z0-9.][a-zA-Z0-9]@[a-zA-Z]+([.][a-zA-Z0-9]+)+";
+	String regex="[a-zA-Z0-9.][a-zA-Z0-Hi9]@[a-zA-Z]+([.][a-zA-Z0-9]+)+";
 	Pattern p=Pattern.compile(regex);
 	Matcher m=p.matcher(str1);
 	if(m.find())
@@ -54,7 +54,22 @@ public static void validemail(String str1)
 
 }
 
+public static void validnumber(String number )
+{
+	String check="(0/91)?[7-9][0-9]{9}";
+	Pattern p=Pattern.compile(check);
+	Matcher m=p.matcher(number);
+	if(m.find())
+	{
+		System.out.println("yes valid emailid ");
+	}
+	else 
+	{
+		System.out.println(" no not a valid mailid ");
+	}
 	
+
+}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner s=new Scanner(System.in);
@@ -64,10 +79,14 @@ public static void validemail(String str1)
 		String lastname=s.nextLine();
 		System.out.println("please enter the emailid");
 		String email=s.nextLine();
+		
+		System.out.println("please enter the mobile number  ");
+		String mobnumber=s.nextLine();
 		 validfirst(firstname);
 		 
 		 validlast(lastname);
 		 validemail(email);
+		 validnumber(mobnumber);
 		 }
 	}
 		
